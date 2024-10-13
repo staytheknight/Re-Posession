6f67108562b6c29554d5e634f6b373575efb6b60
Elise Chevalier <staytheknight@gmail.com>
2024-10-13 13:31:48 -0600

    Reduced turn jitter for agents
    Added a time accumulator that changes the direction of the agent turn after a certain time threshold has been hit - added variation to this time threshold.
    
    Reduced turn radius variation for smoother turns

--- Assets/Core/Agents/Scripts/NavScript.cs
+++ Assets/Core/Agents/Scripts/NavScript.cs
@@ -12,12 +12,18 @@ public class NavScript : MonoBehaviour
     public GameObject vc;
     public Vision visionScript;
 
-    Transform playerTransform;
+    Transform playerTransform;                                      // Transform of the player obj
 
-    float movementRotationRange = 25.0f;
-    Transform parentTransform;
-    public Vector3 targetDestination;
-    private float targetRadius = 2.0f;
+    Transform parentTransform;                                      // Transform of the obj this script is attached to
+    public Vector3 targetDestination;                               // Target destionation for navMesh
+    private float targetRadius = 2.0f;                              // Radius around target position the nav mesh agent will considered arrived
+    float movementRotationRange = 5.0f;                             // Degree in radians the nav mesh agent will rotate it's raycast
+
+    // Time management
+    float[] timeToSwitchTurnRange = {5.0f, 7.0f};                   // Range in seconds for the timeToSwitchTurn variable (will be randomized in range)
+    float timeToSwitchTurn = 5.0f;                                  // How much time in seconds before nav mesh agent will switch turning (left or right)
+    float timeSinceLastFrame = 0.0f;                                // Time in seconds since last frame
+    float timeAccumulator = 0.0f;                                   // Frame time accumulator
 
     // Start is called before the first frame update
     void Start()
@@ -50,15 +56,29 @@ public class NavScript : MonoBehaviour
         // If the agent is close enough to its target destination, find a new target
         if (Mathf.Abs(parentTransform.position.x - targetDestination.x) <= targetRadius &&
             Mathf.Abs(parentTransform.position.y - targetDestination.y) <= targetRadius)
-        {
+        {   
+
+            // Accumulates times between frames, changes direction once accumulator reaches certain threshold
+            timeAccumulator = Time.time + timeSinceLastFrame;
+            if(timeAccumulator > timeToSwitchTurn)
+            {
+                movementRotationRange *= -1.0f;
+                timeAccumulator = 0.0f;
+                // Gets a random value between the ranges of given seconds - this determines how long before the agent switches it's turn direction
+                timeToSwitchTurn = Random.Range(timeToSwitchTurnRange[0], timeToSwitchTurnRange[1]);
+            }
+            timeSinceLastFrame = Time.time;
+
             // Rotate the transform towards a direction in the given range
-            parentTransform.Rotate(0,Random.Range(movementRotationRange, -movementRotationRange),0);
+            parentTransform.Rotate(0,Random.Range(0, movementRotationRange),0);
             // Casts a ray towards that direction with infinite length
             Physics.Raycast(parentTransform.position, parentTransform.forward, out var hitInfo);
             // Move towards point hit by RayCast
             agent.destination = hitInfo.point;
             targetDestination = hitInfo.point;
             //clickIndicator.displayClickIndicator(hitInfo.point, true);   // Debug visual
+
+            
         }
     }
 }
