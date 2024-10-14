756d6f2603b87cd82db3051a46f3d57ff5540970
Elise Chevalier <staytheknight@gmail.com>
2024-10-14 11:30:13 -0600

    Agent colour change with eye colour change
    Other changes include adding small point lights around each agent and player, and adding transparency materials

--- Assets/Core/Agents/Scripts/EyeColourManager.cs
+++ Assets/Core/Agents/Scripts/EyeColourManager.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class EyeColourManager : MonoBehaviour
 {
     Light[] lights;
+    Renderer meshRenderer;
+
+    // This would be better as a dictionary, I just didn't have enough time to figure it out
+    public Material red;
+    public Material green;
+    public Material magenta;
+
     // Start is called before the first frame update
     void Start()
     {
-        lights = this.GetComponentsInChildren<Light>();        
+        lights = this.GetComponentsInChildren<Light>();
+        meshRenderer = this.GetComponent<Renderer>();
     }
 
     public void changeLightColour(Color colour)
@@ -17,6 +25,21 @@ public class EyeColourManager : MonoBehaviour
         {
             lights[i].color = colour;
         }
+
+        // This is better as a dictionary (see material definitions above)
+        if (colour == Color.white)
+        {
+            changeMeshMaterial(red);
+        }
+        else if (colour == Color.green)
+        {
+            changeMeshMaterial(green);  
+        }
+        else if (colour == Color.magenta)
+        {
+            changeMeshMaterial(magenta);
+        }
+        
     }
 
     public Color[] getLightColours()
@@ -28,5 +51,10 @@ public class EyeColourManager : MonoBehaviour
         }
 
         return cLights;
+    }
+
+    void changeMeshMaterial(Material m)
+    {
+        meshRenderer.material = m;
     }
 }
