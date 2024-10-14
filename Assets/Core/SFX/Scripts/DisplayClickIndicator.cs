56ba698ed26798152466fb6a6dbea8d282d551f0
Elise Chevalier <staytheknight@gmail.com>
2024-10-14 11:07:57 -0600

    Click indicator visual (sprite)
    Changed clicked indicator visual from mesh to sprite, changed code for mesh material change to sprite colour change on double click

---
+++ Assets/Core/SFX/Scripts/DisplayClickIndicator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayClickIndicator : MonoBehaviour
+{
+    public GameObject clickLocObject;
+
+    public void displayClickIndicator(Vector3 point, bool doubleClick)
+    {
+        destroyClickIndicators();
+
+        switchColour(doubleClick);
+        Instantiate(clickLocObject, point, Quaternion.identity);
+    }
+
+    void destroyClickIndicators()
+    {
+        GameObject[] clickIndicators = GameObject.FindGameObjectsWithTag("Click Indicator");
+        foreach (GameObject obj in clickIndicators)
+        {
+            Destroy(obj);
+        }
+    }
+
+    void switchColour(bool doubleClicked)
+    {
+        if(doubleClicked)
+        {
+            Debug.Log("double clicked");
+            clickLocObject.GetComponentInChildren<SpriteRenderer>().color = Color.blue;
+        }
+        else
+        {
+            Debug.Log("single clicked");
+            clickLocObject.GetComponentInChildren<SpriteRenderer>().color = Color.white;
+        }
+    }
+}
